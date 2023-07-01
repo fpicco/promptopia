0a061d97170877edeec180af27e42ac14af2db28
fpicco <flor.picco@gmail.com>
2023-07-01 10:23:07 -0300

    Add render posted prompts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+'use client';
+
+import { useEffect, useState } from 'react';
+import PromptCard from './PromptCard';
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
 
-export default Feed
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      setPosts(data);
+      console.log(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+      <div className='mt-16 prompt_layout'>
+        {posts.map((post) => {
+          return (
+            <PromptCard
+              key={post._id}
+              post={post}
+              // handleTagClick={handleTagClick}
+            />
+          )
+        })}
+      </div>
+      
+    </div>
+  );
+};
+
+export default Feed;
